21b4fc7af0eaff5f285e36dffe1e41171b6a9da3
AureteProgrammer <CodingBlakeMorris@gmail.com>
2026-04-09 12:20:40 -0400

    {fetching API}

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,3 +1,14 @@
-fetch('https://dummyjson.com/products')
-.then(res => res.json())
-.then(console.log);
+export async function fetchProducts() {
+    try {
+        const response = await fetch('https://dummyjson.com/products');
+        
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        
+        const data = await response.json();
+        return data.products; 
+    } catch (error) {
+        throw error; 
+    }
+}
