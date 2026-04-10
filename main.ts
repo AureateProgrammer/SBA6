df08daa87eff1109391f7c570a24e5bfe7a7910c
AureteProgrammer <CodingBlakeMorris@gmail.com>
2026-04-10 09:11:29 -0400

    Starting main

--- main.ts
+++ main.ts
@@ -2,3 +2,18 @@ import { fetchProducts } from './services/apiService';
 import { Product } from './models/Product';
 import { calculateTax } from './utils/taxCalculator';
 import { handleError } from './utils/errorHandler';
+
+async function startApp() {
+    try {
+        const rawProducts = await fetchProducts();
+
+        const productInstances = rawProducts.map((item: any) => {
+            return new Product(
+                item.title,
+                item.description,
+                item.price,
+                item.discountPercentage,
+                item.category
+            );
+        });
+        
